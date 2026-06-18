de12c1bbf676eebb859596c72f98a146faff09ce
arif saputra <ArifYogiSaputra_1701620136@mhs.unj.ac.id>
2026-06-18 17:48:49 +0700

    Update WhatsApp contact number

--- config/public.ts
+++ config/public.ts
@@ -1,7 +1,7 @@
 export const publicContact = {
 	primaryPhone: '+62 812-1315-4039',
 	primaryPhoneHref: '+6281213154039',
-	whatsappPhone: '6281213154039',
+	whatsappPhone: '628212134261',
 };
 
 export const publicWhatsAppUrl = `https://api.whatsapp.com/send?phone=${publicContact.whatsappPhone}`;
